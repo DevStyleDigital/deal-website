457d52c5a68014a67e644bb393ce854cb26bf032
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2023-12-18 14:24:35 -0300

    first commit

--- src/app/(app)/enterprise-emphasis.tsx
+++ src/app/(app)/enterprise-emphasis.tsx
@@ -16,12 +16,12 @@ export const EnterpriseEmphasis = ({
       </span>
       <h1 className="text-center">Inimaginável</h1>
     </div>
-    <div className="mx-auto max-w-7xl px-8 flex max-lg:flex-col p-4 relative">
+    <div className="mx-auto max-w-7xl px-8 flex min-h-min max-lg:flex-col p-4 relative">
       <span
         aria-hidden
         className="border-gold border-r-[1px] border-t-[1px] w-20 h-20 absolute top-0 right-4"
       />
-      <div className="w-full max-xl:relative">
+      <div className="w-full h-full flex-1 max-xl:relative">
         {!!enterpriseEmphasis.banner_emphasis.url && (
           <Image
             alt={enterpriseEmphasis.banner_emphasis?.label || ''}
@@ -29,7 +29,7 @@ export const EnterpriseEmphasis = ({
             width={2860}
             height={1080}
             priority
-            className="w-full lg:h-full h-[300px] object-cover object-center"
+            className="w-full lg:h-[620px] h-[300px] object-cover object-center"
           />
         )}
         <span
@@ -37,20 +37,22 @@ export const EnterpriseEmphasis = ({
           className="border-gold border-l-[1px] border-b-[1px] w-20 h-20 absolute -left-4 -bottom-4 xl:left-4 xl:bottom-0"
         />
       </div>
-      <div className="space-y-6 w-full p-8 py-10 xl:max-w-sm max-lg:mt-8 bg-blue !text-white [&_p]:!text-white">
-        <h1 className="border-gold text-gold border-l-[1px] pl-4">
-          <span className="text-3xl block truncate xl:max-w-xs">
-            {enterpriseEmphasis.name}
-          </span>
-          <span className="text-2xl block truncate xl:max-w-xs">
-            {enterpriseEmphasis.type}
-          </span>
-        </h1>
-        <div className="flex items-center space-x-4">
-          <MapPin />
-          <p>
-            {enterpriseEmphasis.city} / {enterpriseEmphasis.state}
-          </p>
+      <div className="space-y-10 w-full p-8 py-10 xl:max-w-sm max-lg:mt-8 bg-blue !text-white [&_p]:!text-white flex flex-col">
+        <div className="space-y-6">
+          <h1 className="border-gold text-gold border-l-[1px] pl-4">
+            <span className="text-3xl block truncate xl:max-w-xs">
+              {enterpriseEmphasis.name}
+            </span>
+            <span className="text-2xl block truncate xl:max-w-xs">
+              {enterpriseEmphasis.type}
+            </span>
+          </h1>
+          <div className="flex items-center space-x-4">
+            <MapPin />
+            <p>
+              {enterpriseEmphasis.city} / {enterpriseEmphasis.state}
+            </p>
+          </div>
         </div>
         <p>{enterpriseEmphasis.desc}</p>
 
